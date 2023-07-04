113192adb13f680181c9a3065ed0aaa5cc6d82aa
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-07-04 12:57:07 -0700

    Phone number delivery active

--- src/routes/webhook/+server.ts
+++ src/routes/webhook/+server.ts
@@ -132,7 +132,7 @@ export async function POST({ request }: RequestEvent) {
             //     : `https://turnt.party/qrCode/${paymentIntentSucceeded.id}`
             // }`,
             body: `Turnt - Here is your ticket Qr Code: www.turnt.party/qrCode/${paymentIntentSucceeded.id}`,
-            from: "+18663958046",
+            from: "+18185930804",
             to: `${paymentIntentSucceeded.metadata.purchaserPhoneNumber}`,
           })
           .then((message: any) => console.log(message.sid));
