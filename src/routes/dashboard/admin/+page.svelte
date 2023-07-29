25cc5c1db395e9763136b09d5d908a639c7b367b
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-07-29 01:18:18 -0700

    Embarassing

--- src/routes/dashboard/admin/+page.svelte
+++ src/routes/dashboard/admin/+page.svelte
@@ -13,7 +13,7 @@
 
   const getPoshParties = async () => {
     try {
-      const res = await axios.get("https://turnt-server.fly.dev/");
+      const res = await axios.get("https://turnt-server.fly.dev/getPosh");
       partiesToImport = res.data;
       console.log(partiesToImport);
     } catch (err) {
